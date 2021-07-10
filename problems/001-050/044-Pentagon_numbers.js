ea3ab66dc3f237046ee9f6f75f1bc17476682639
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2021-07-10 13:01:08 +0200

    problem 44, 45

--- problems/001-050/044-Pentagon_numbers.js
+++ problems/001-050/044-Pentagon_numbers.js
@@ -28,9 +28,9 @@ this.solve = function () {
   const pairIsPenta = (Pj,Pk) => (Pj+Pk) in pentaHT && (Pk-Pj) in pentaHT;
 
   // First we generate some pentagonal numbers. The limit is set arbitrarily so
-  // that we can get some pairs in a reasonable time.
+  // that we can get some pairs in a reasonable time (cheating a bit).
   const maxPn = 10**8; // p(n) = n(3n−1)/2 ⟹ 3n² − n − 2p(n) = 0
-  const limit = Math.ceil(1+Math.sqrt(1+24*maxPn)/6);
+  const limit = Math.ceil((1+Math.sqrt(1+24*maxPn))/6);
 
   for (let n=1; n<limit; n++) {
     const Pn = P(n);
