27e4a47535f9b62223601f1c714a0629db80213d
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2021-08-19 13:50:45 +0200

    problem 75

---
+++ problems/051-100/075-Singular_integer_right_triangles.js
@@ -0,0 +1,63 @@
+/**
+ * Problem 75 - Singular integer right triangles
+ *
+ * @see {@link https://projecteuler.net/problem=75}
+ *
+ * It turns out that 12 cm is the smallest length of wire that can be bent to
+ * form an integer sided right angle triangle in exactly one way, but there are
+ * many more examples.
+ *
+ *    12 cm: (3,4,5)
+ *    24 cm: (6,8,10)
+ *    30 cm: (5,12,13)
+ *    36 cm: (9,12,15)
+ *    40 cm: (8,15,17)
+ *    48 cm: (12,16,20)
+ *
+ * In contrast, some lengths of wire, like 20 cm, cannot be bent to form an
+ * integer sided right angle triangle, and other lengths allow more than one
+ * solution to be found; for example, using 120 cm it is possible to form
+ * exactly three different integer sided right angle triangles.
+ *
+ *    120 cm: (30,40,50), (20,48,52), (24,45,51)
+ *
+ * Given that L is the length of the wire, for how many values of L ≤ 1,500,000
+ * can exactly one integer sided right angle triangle be formed ?
+ */
+
+const { PPTp } = require('../../lib/triangle');
+
+this.solve = function () {
+
+  // We already had to deal with pythagorean triples so the problem is quite
+  // easy now that we got the proper function.
+
+  // We will just generate all the pythagorean triples for which the perimeter
+  // stays under the given limit and count those appearing only once.
+
+  // This probabaly could be done more efficiently though.
+
+  const L = 1_500_000;
+  const ppt = PPTp(L);
+
+  let LValues = {};
+  let single = new Set();
+
+  for (let i=0; i<ppt.length; i++) {
+    const [a, b, c] = ppt[i];
+    const p = a + b + c;
+    let m = 1, l;
+    while ((l = p*m++) <= L) {
+      if (l in LValues) {
+        LValues[l]++;
+        single.delete(l);
+      }
+      else {
+        LValues[l] = 1;
+        single.add(l);
+      }
+    }
+  }
+
+  return single.size;
+}
