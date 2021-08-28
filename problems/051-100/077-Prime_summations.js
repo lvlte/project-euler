f1e22b7377604e097f35a85a8b0777db1c2f0dd4
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2021-08-28 16:48:09 +0200

    Alternative method for problem 77 (generating function)

--- problems/051-100/077-Prime_summations.js
+++ problems/051-100/077-Prime_summations.js
@@ -78,4 +78,80 @@ this.solve = function () {
   while (pn <= nWays && n++);
 
   return n;
-}
+};
+
+
+this.solve2 = function () {
+
+  // Another method using the generating function :
+
+  // Let P a set of prime numbers, then the number of partitions of n whose
+  // parts belong to P has the following generating function :
+  //
+  //  Π[p∈P](1-x^p)^-1
+
+  // A generating function is a single function used to "encode" an infinite
+  // sequence. The output of such function is not the nth term of the sequence
+  // but a power series whose coefficients are the actual terms.
+  // @see http://discrete.openmathbooks.org/dmoi2/section-27.html
+
+  // The generating function (1-x)^-1 yields the series :
+  //  1 + x + x² + x³ + x⁴ + ...
+  // which corresponds to the sequence of coefficients :
+  //  1, 1, 1, 1, 1, ...
+
+  // The sequence for (1-x²)^-1 = 1 + x² + x⁴ + x⁶ + ... is :
+  //  1, 0, 1, 0, 1, 0, 1, ...
+
+  // The sequence for (1-x³)^-1 = 1 + x³ + x⁶ + x⁹ + ... is :
+  //  1, 0, 0, 1, 0, 0, 1, 0, 0, 1, ...
+
+  // -> There is a simple pattern to exploit here :
+  //  Given (1-x^p)^-1, we got the pattern `one, followed by (p-1) zeros` which
+  //  repeats indefinitely.
+
+  // We are given Π[p∈P](1-x^p)^-1, wich corresponds to the product of the power
+  // series of (1-x^p)^-1 for p the prime parts to be used. This means there is
+  // a generating function and its corresponding series for each prime parts,
+  // and we need to compute the product of these generating functions.
+
+  // An infinite power series is an infinite sum of terms. We can't manipulate
+  // infinite sequences, so we need to set an arbitrary limit for the number of
+  // terms to work out. Not an issue as we just need to expand the power series
+  // of the generating function until some coefficient exceeds five thousands.
+  // So we just need to adjust this limit accordingly.
+
+  const nWays = 5_000;
+  const limit = 100;
+  const primes = getPrimes(limit);
+
+  // Computes the product of the given sequences A and B.
+  // S = a0b0 + (a0b1+a1b0)x + (a0b2+a1b1+a2b0)x² + (a0b3+a1b2+a2b1+a3b0)x³ + ⋯
+  const sequenceProduct = (A, B) => {
+    let S = Array(limit);
+    S[0] = 1; // a0b0 is a constant term as x^0 = 1
+    for (let i=1; i<limit; i++) {
+      S[i] = 0;
+      for (let k=0; k<=i; k++)
+        S[i] += A[k] * B[i-k];
+    }
+    return S;
+  };
+
+  // Initilaize the sequence of coefficients given by our power series starting
+  // with the smallest prime, as shown above : 1, 0, 1, 0, 1, 0, 1, ...
+  let S = Array(limit).fillS([1, 0]);
+
+  // Now for each primes p, we turn (1-x^p)^-1 into the corresponding sequence
+  // and update the sequences product accordingly.
+  for (let i=1; i<primes.length; i++) {
+    const pattern = [1, ...Array(primes[i]-1).fill(0)];
+    S = sequenceProduct(S, Array(limit).fillS(pattern));
+  }
+
+  const n = S.findIndex(term => term > nWays);
+  if (n === -1)
+    return [undefined, 'Need to increase `limit`'];
+
+  return n;
+};
