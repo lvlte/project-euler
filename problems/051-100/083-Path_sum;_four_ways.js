8638a3ba1751fae52e1a7c1a251eb8c90f262926
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2021-09-03 16:46:06 +0200

    Graph data structure / Working out adjacency lists and reduced distance matrices from a given (path) grid.

--- problems/051-100/083-Path_sum;_four_ways.js
+++ problems/051-100/083-Path_sum;_four_ways.js
@@ -18,9 +18,10 @@
  * 80 by 80 matrix.
  */
 
+const { adjListFromGrid } = require('../../lib/graph');
 const { load } = require('../../lib/utils');
 const data = load('p083_matrix.txt').split(/\r\n|\n/);
-const matrix = data.filter(l => l).map(r => r.split(',').map(n => +n));
+// const matrix = data.filter(l => l).map(r => r.split(',').map(n => +n));
 
 this.solve = function () {
   // This is known as a "Shortest path problem".
@@ -30,25 +31,14 @@ this.solve = function () {
   // between two vertices (or nodes) in a graph such that the sum of the weights
   // of its constituent edges is minimized.
 
-  // The approach would be to consider our matrix as an input for an adjacency
-  // matrix, or as a distance matrix. In graph theory and computer science,
-  // -> an adjacency matrix is a square matrix used to represent a finite graph.
-  // The elements of the matrix indicate whether pairs of vertices are adjacent
-  // or not in the graph.
-  // -> a distance matrix is a square matrix (two-dimensional array) containing
-  // the distances, taken pairwise, between the elements of a set.
-
-  // In graph-theoretic applications the elements are more often referred to as
+  // In graph-theoretic applications, the elements are more often referred to as
   // points, nodes or vertices. Other common terms :
   //  - intersection = vertex
   //  - road         = edge
   //  - map          = graph
 
-  // So we need to transform our matrix into a graph such that each cell becomes
-  // a vertex and has an edge to the cell above, below, and to the right.
-
-  // How to represent graph ?
-  // https://www.khanacademy.org/computing/computer-science/algorithms/graph-representation/a/representing-graphs
+  // We need to transform our matrix into a graph such that each cell becomes a
+  // vertex and has an edge to the cell above, below, and to the right.
 
   // Then we can use a shortest path algorithm, the most commonly used are :
   // - Dijkstra's original algorithm
@@ -56,5 +46,25 @@ this.solve = function () {
   // - A* search algorithm (basically Dijkstra's with an heuristic function)
   //    http://csis.pace.edu/~benjamin/teaching/cs627/webfiles/Astar.pdf
 
+  // How to represent graph ?
+  // https://www.khanacademy.org/computing/computer-science/algorithms/graph-representation/a/representing-graphs
 
+  // We will represent our graph as an adjacency list, such that each vertex v
+  // is mapped to a list of all the vertices adjacent to it.
+
+  const matrix = [
+    [131, 673, 234, 103, 18],
+    [201, 96, 342, 965, 150],
+    [630, 803, 746, 422, 111],
+    [537, 699, 497, 121, 956],
+    [805, 732, 524, 37, 331]
+  ];
+
+  const [graph, distMx] = adjListFromGrid(matrix);
+
+  console.log(graph);
+  console.log(graph.length);
+
+  console.log(distMx);
+  console.log(distMx.length);
 }
