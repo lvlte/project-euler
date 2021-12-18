277d97cc1f65f443841113420acfa3b86eb8490a
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2021-12-18 16:09:07 +0100

    add problem 94

---
+++ problems/051-100/094-Almost_equilateral_triangles.js
@@ -0,0 +1,113 @@
+/**
+ * Problem 94 - Almost equilateral triangles
+ *
+ * @see {@link https://projecteuler.net/problem=94}
+ *
+ * It is easily proved that no equilateral triangle exists with integral length
+ * sides and integral area. However, the almost equilateral triangle 5-5-6 has
+ * an area of 12 square units.
+ *
+ * We shall define an almost equilateral triangle to be a triangle for which two
+ * sides are equal and the third differs by no more than one unit.
+ *
+ * Find the sum of the perimeters of all almost equilateral triangles with
+ * integral side lengths and area and whose perimeters do not exceed one
+ * billion (1,000,000,000).
+ */
+
+this.solve = function () {
+  // A Heronian triangle, also known as a Heron triangle or a Hero triangle, is
+  // a triangle with integer sides and integer area.
+
+  // We are searching for Heronian triangles satisfying the "almost equilateral"
+  // definition, that is all isosceles Hero triangles for which the base length
+  // differs from the legs length by exactly one unit.
+
+  // The perimeter of a Heronian triangle is always an even number, thus every
+  // Heronian triangle has an odd number of sides of even length.
+
+  // All isosceles Heronian triangles are decomposable. They are formed by
+  // joining two congruent Pythagorean triangles along either of their common
+  // legs such that the equal sides of the isosceles triangle are the
+  // hypotenuses of the Pythagorean triangles, and the base of the isosceles
+  // triangle is twice the other Pythagorean leg.
+
+  // Consequently, every PT is the building block for two isosceles Heronian
+  // triangles since the join can be along either leg. Given a PT (a, b, c) :
+  //  H1 = (a, b, c) (joining on b)
+  //  H2 = (a, b, c) (joining on a)
+
+  // Therefore, since an almost equilateral triangle is of the form (c, c, c±1),
+  // given a pythagorean triple (a, b, c) :
+  //  -> If 2a = c ± 1, then H1 is an almost equilateral triangle.
+  //  -> If 2b = c ± 1, then H2 is an almost equilateral triangle.
+  //  -> c must be odd for the number of sides of even length to be odd.
+
+  // This means that all such triangles can be built from primitive pythagorean
+  // triangles and that no derivative can match (since c is odd and 2a is even,
+  // |2a - c| >= 1, then for any positive integer m, m * |2a - c| >= m, so the
+  // 1-unit length difference can only hold when |2a - c| = 1 with m = 1).
+
+  // Given c, the hypotenuse of a pythagorean triangle, a corresponding almost
+  // equilateral triangle has a perimeter p = 3*c ± 1, so we can define cMax,
+  // the maximum hypotenuse length, such that 3*cMax + 1 = pMax.
+
+  // However, generating all primitive pythagorean triples with an hypotenuse
+  // lower than (10^9-1)/3 (which means a lot) and checking for each one of them
+  // if |2a - c| = 1 or |2b - c| = 1 is not efficient at all.
+
+  // Instead, since we already got two arbitrary triples of the form (c, c, c±1)
+  // for any odd integer c such that cMin <= c <= cMax, then for each c we can
+  // set a = (c±1)/2, b = √(c² - a²), and check whether or not b is an integer.
+  // In such case, (a, b, c) is a pythagorean triple and therefore (c, c, c±1)
+  // is an almost equilateral heronian triangle.
+
+  const pMax = 1_000_000_000;   // maximum perimeter
+  const cMax = (pMax - 1) / 3;  // maximum hypotenuse length
+  const cMin = 5;               // smallest triple is (3, 4, 5)
+
+  // The max safe value for c is the max safe root of c².
+  const rtMax = Math.sqrt(Number.MAX_SAFE_INTEGER);
+
+  // Checks if (a, b, c) is a pythagorean triple.
+  const check = (a, b, c, a2, b2, c2) => {
+    if (Number.isInteger(b) && a2 + b2 - c2 === 0) {
+      if (c <= rtMax)
+        return true;
+      // For c greater than max safe root, we need to double check with bigint
+      // to ensure we have no error.
+      c = BigInt(c);
+      a = BigInt(a);
+      b = Math.sqrt(Number(c*c - a*a));
+      if (!Number.isInteger(b))
+        return false;
+      b = BigInt(b);
+      return a*a + b*b === c*c;
+    }
+    return false;
+  }
+
+  // The sum of perimeters of all almost equilateral Heronian triangles
+  let pSum = 0;
+
+  // For each odd value of c, we consider the triangle (c, c, c±1) and check if
+  // it can be decomposed into two copy of a pythagorean triples (a, b, c), in
+  // which case it is an almost equilateral heronian triangle.
+  for (let c=cMin; c<=cMax; c+=2) {
+    let a = (c-1)/2;    // (c, c, c-1)
+    let a2 = a*a;
+    let c2 = c*c;
+    let b = Math.sqrt(c2 - a2);
+    if (check(a, b, c, a2, b*b, c2)) {
+      pSum += 3*c - 1;
+      continue;
+    }
+    a = a+1;            // (c, c, c+1)
+    a2 = a*a;
+    b = Math.sqrt(c2 - a2);
+    if (check(a, b, c, a2, b*b, c2))
+      pSum += 3*c + 1;
+  }
+
+  return pSum;
+}
