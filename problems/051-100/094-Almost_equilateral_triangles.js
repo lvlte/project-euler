88f51fcb5a85e528ec9479a4eac7b536557cbfe5
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2021-12-20 20:38:08 +0100

    problem 94 - 2nd approach (more efficient)

--- problems/051-100/094-Almost_equilateral_triangles.js
+++ problems/051-100/094-Almost_equilateral_triangles.js
@@ -15,6 +15,8 @@
  * billion (1,000,000,000).
  */
 
+const { PPTChildT3 } = require('../../lib/triangle');
+
 this.solve = function () {
   // A Heronian triangle, also known as a Heron triangle or a Hero triangle, is
   // a triangle with integer sides and integer area.
@@ -56,9 +58,84 @@ this.solve = function () {
   // lower than (10^9-1)/3 (which means a lot) and checking for each one of them
   // if |2a - c| = 1 or |2b - c| = 1 is not efficient at all.
 
-  // Instead, since we already got two arbitrary triples of the form (c, c, c±1)
-  // for any odd integer c such that cMin <= c <= cMax, then for each c we can
-  // set a = (c±1)/2, b = √(c² - a²), and check whether or not b is an integer.
+  // (first attempt to solve this without ppt is described below in solve2)
+
+  // Hopefully, Berggren showed that all primitive Pythagorean triples can be
+  // generated from the (3, 4, 5) triangle by using 3 linear transformations
+  // (T1, T2, T3). Hence, each one of them is the "parent" of three additional
+  // primitive triples :
+
+  //      |        a      |         b      |         c
+  //  ----|---------------|----------------|-----------------
+  //   T1 |  a - 2b + 2c  |   2a - b + 2c  |   2a - 2b + 3c
+  //  ----|---------------|----------------|-----------------
+  //   T2 |  a + 2b + 2c  |   2a + b + 2c  |   2a + 2b + 3c
+  //  ----|---------------|----------------|-----------------
+  //   T3 | -a + 2b + 2c  |  -2a + b + 2c  |  -2a + 2b + 3c
+
+  // ... and if we inspect these parent/child relationships, it appears that all
+  // triples that satisfy the condition belong to the same (unique) lineage :
+  // - triples satisfying |2a - c| = 1 have one T3-child satisfying |2b - c| = 1
+  // - triples satisfying |2b - c| = 1 have one T1-child satisfying |2a - c| = 1
+  //
+  //    Suppose we obtain a triple for which |2a - c| = 1 by applying T1 on its
+  //    parent, it implies the parent satifies :
+  //     |2(a - 2b + 2c) - (2a - 2b + 3c)| = 1
+  //     |-2b + c| = 1
+  //     |2b - c| = 1
+  //
+  //    And if we obtain a triple for which |2b - c| = 1 by applying T3 on its
+  //    parent, it implies the parent satifies :
+  //     |2(-2a + b + 2c) - (-2a + 2b + 3c)| = 1
+  //     |-2a + c| = 1
+  //     |2a - c| = 1
+
+  // This is rather convenient for our problem !
+
+  // With the same logic, we can prove that transition T2 can't produce such
+  // triple whatever the parent, and also that there is no transition that can
+  // produce such triple if the parent doesn't meet the requirement in the first
+  // place (except obviously for the root).
+
+  // In other words, starting from root (a=3, b=4, c=5) for which |2a - c| = 1,
+  // and by applying the linear transformations T3 and T1 alternatively, we will
+  // obtain every pythagorean triples that are the building blocks of an almost
+  // equilateral triangle, and only them.
+
+  // Also, as T3(a, b, c) and T1(b, a, c) yield the same triple with the output
+  // values of a and b swapped, we can use only one transition and swap a and b
+  // between each call. This way, we only have to consider the case |2a - c| = 1
+  // and the corresponding triangle (c, c, 2a) with perimeter p = 2(c+a) because
+  // our variable `a` will represent alternatively a and b.
+
+  const pMax = 1_000_000_000;   // maximum perimeter
+  const cMax = (pMax - 1) / 3;  // maximum hypotenuse length
+
+  // The sum of perimeters of all almost equilateral Heronian triangles
+  let pSum = 0;
+  let [a, b, c] = [3, 4, 5];
+
+  while (c <= cMax) {
+    pSum += 2*(c+a);
+    [b, a, c] = PPTChildT3(a, b, c);
+  }
+
+  return pSum;
+}
+
+// Alternative method
+this.solve2 = () => {
+  // Originally, my approach was to avoid having to generate pythagorean triples
+  // but still to take advantage of the heronian triangles being decomposable in
+  // two pythagorean triples. This is simpler but involves more code and is less
+  // efficient.
+
+  // Since we already got two arbitrary triples of the form (c, c, c±1) for any
+  // odd integer c such that cMin <= c <= cMax, then for each c we can set :
+  //  a = (c ± 1)/2
+  //  b = √(c² - a²)
+  // and check whether or not b is an integer.
+
   // In such case, (a, b, c) is a pythagorean triple and therefore (c, c, c±1)
   // is an almost equilateral heronian triangle.
 
